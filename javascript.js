b8ed3d96bce4cf47ffd67df3790710fa74b9f934
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-08-19 16:00:27 -0700

    match password fields and add phone field pattern

---
+++ javascript.js
@@ -0,0 +1,33 @@
+const password = document.getElementById("user_password");
+const confirmPassWord = document.getElementById("user_password_confirm");
+const phoneNumber = document.getElementById("phone_number");
+let passwordValue = "";
+let confirmPasswordValue = "";
+let phoneNumberValue = "";
+let phoneNumberFiltered = [];
+const myRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/g;
+
+window.addEventListener("click", () =>{
+    passwordValue = password.value;
+    confirmPasswordValue = confirmPassWord.value;
+    phoneNumberValue = phoneNumber.value;
+    phoneNumberFiltered = myRegex.exec(phoneNumberValue);
+
+    console.log(phoneNumberValue);
+    console.log(phoneNumberFiltered);
+
+    if(passwordValue!= confirmPasswordValue){
+        confirmPassWord.style.borderBottomColor = "red";
+        confirmPassWord.style.borderRightColor = "red";
+        confirmPassWord.setCustomValidity("Passwords do not match");
+    
+    }
+    else{
+        confirmPassWord.style.borderBottomColor = "rgb(118, 118, 118)";
+        confirmPassWord.style.borderRightColor = "rgb(118, 118, 118)";
+
+    }
+
+    
+});
+
